80dacf9ca9d86006b227a796b7714a70359a19fc
Atakan Yontar <atakanyontar@gmail.com>
2021-09-26 18:54:50 +0300

    solved 2 hackerrank questions

---
+++ Algorithms/AVeryBigSum.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+public class AVeryBigSum {
+    public static long aVeryBigSum(List<Long> ar) {
+        // Write your code here
+        long result = 0;
+        for (int i = 0; i < ar.size(); i++) {
+            result += ar.get(i);
+        }
+        return result;
+    }
+}
+
+/*
+ * QUESTION EXPLANATION
+ * 
+ * In this challenge, you are required to calculate and print the sum of the
+ * elements in an array, keeping in mind that some of those integers may be
+ * quite large.
+ * 
+ * Function Description
+ * 
+ * Complete the aVeryBigSum function in the editor below. It must return the sum
+ * of all array elements.
+ * 
+ * Sample Input
+ * 
+ * 5 1000000001 1000000002 1000000003 1000000004 1000000005
+ * 
+ * Output
+ * 
+ * 5000000015
+ * 
+ */
---
+++ Algorithms/DiagonalDifference.java
@@ -0,0 +1,59 @@
+import java.util.List;
+
+public class DiagonalDifference {
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        int size = arr.size();
+        int rIndex = 0;
+        int lIndex = size - 1;
+        int result = 0;
+        for (int i = 0; i < size; i++) {
+            result += arr.get(i).get(rIndex++);
+            result -= arr.get(i).get(lIndex--);
+        }
+        return Math.abs(result);
+    }
+}
+
+/*
+ * Function description
+ * 
+ * Complete the function in the editor below.
+ * 
+ * diagonalDifference takes the following parameter:
+ * 
+ * int arr[n][m]: an array of integers
+ * 
+ * Return
+ * 
+ * int: the absolute diagonal difference
+ * 
+ * Output Format
+ * 
+ * Return the absolute difference between the sums of the matrix's two diagonals
+ * as a single integer.
+ * 
+ * Sample Input
+ * 
+ * 3 11 2 4 4 5 6 10 8 -12
+ * 
+ * Sample Output
+ * 
+ * 15
+ * 
+ * Explanation
+ * 
+ * The primary diagonal is:
+ * 
+ * 11 5 -12
+ * 
+ * Sum across the primary diagonal: 11 + 5 - 12 = 4
+ * 
+ * The secondary diagonal is:
+ * 
+ * 4 5 10
+ * 
+ * Sum across the secondary diagonal: 4 + 5 + 10 = 19
+ * 
+ * Difference: |4 - 19| = 15
+ * 
+ */
