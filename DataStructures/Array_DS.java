fe830ec939b0b6e7bae61bf416724b53bfb61d66
Atakan Yontar <atakanyontar@gmail.com>
2021-09-28 17:53:34 +0300

    array_ds | solved

---
+++ DataStructures/Array_DS.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Array_DS {
+    public static List<Integer> reverseArray(List<Integer> a) {
+        for (int i = 0; i < a.size() / 2; i++) {
+            int temp = a.get(i);
+            a.set(i, a.get(a.size() - (i + 1)));
+            a.set(a.size() - (i + 1), temp);
+        }
+
+        return a;
+    }
+}
