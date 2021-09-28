74ec5e6318fae3ba0d2e7fe93d7112ed9cc84cbb
Atakan Yontar <atakanyontar@gmail.com>
2021-09-28 17:51:37 +0300

    package declared

--- Algorithms/AVeryBigSum.java
+++ Algorithms/AVeryBigSum.java
@@ -1,3 +1,5 @@
+package Algorithms;
+
 import java.util.List;
 
 public class AVeryBigSum {
--- Algorithms/Runner.java
+++ Algorithms/Runner.java
@@ -1,3 +1,5 @@
+package Algorithms;
+
 public class Runner {
     public static void main(String[] args) {
 
