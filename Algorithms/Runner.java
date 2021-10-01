b2dd639064fa344d76e82f6b7806a94aaf82160d
Atakan Yontar <atakanyontar@gmail.com>
2021-10-01 14:33:47 +0300

    some work

--- Algorithms/Runner.java
+++ Algorithms/Runner.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
-        Integer[] i = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 1 };
+        Integer[] i = { 3, 4, 7, 5, 6, 2, 1  };
         List<Integer> l1 = Arrays.asList(i);
         InsertionSort_Part1.insertionSort1(10, l1);
     }
