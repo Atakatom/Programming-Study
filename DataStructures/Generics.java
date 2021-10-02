83c296b4c3d3393f8ede122208c80e74684eb142
Atakan Yontar <atakanyontar@gmail.com>
2021-10-02 18:22:08 +0300

    Generics | completed

--- DataStructures/Generics.java
+++ DataStructures/Generics.java
@@ -1,9 +1,9 @@
 package DataStructures;
 
 public class Generics {
-    public static void printArray(E[] arr) {
-        for (int i = 0; i < arr.length(); i++) {
-            System.out.println(arr[i]);
+    public static <E> void printArray(E[] arr) {
+        for (E item : arr) {
+            System.out.println(item);
         }
     }
 }
