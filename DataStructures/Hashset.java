d5a3af833fbde454bc08119c5ac3fca77f6405b0
Atakan Yontar <atakanyontar@gmail.com>
2021-10-02 11:46:07 +0300

    Hashset | completed

---
+++ DataStructures/Hashset.java
@@ -0,0 +1,37 @@
+package DataStructures;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Hashset {
+    public static void Scan() {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String[] pair_left = new String[t];
+        String[] pair_right = new String[t];
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+        findUniquePairs(pair_left, pair_right, t);
+        s.close();
+    }
+
+    public static void findUniquePairs(String[] pair_left, String[] pair_right, int t) {
+        HashSet<String> pairs = new HashSet<String>();
+        for (int j = 0; j < t; j++) {
+            pairs.add(pair_left[j] + " " + pair_right[j]);
+            System.out.println(pairs.size());
+        }
+    }
+    /*
+     * Sample Input
+     * 
+     * 5 john tom john mary john tom mary anna mary anna
+     * 
+     * Sample Output
+     * 
+     * 1 2 2 3 3
+     */
+
+}
