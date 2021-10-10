00e8d2ba1e8662fa0f6bb52f84ba892e6759e09d
Atakan Yontar <atakanyontar@gmail.com>
2021-10-10 17:39:30 +0300

    CompareTheTriplets | completed

---
+++ Algorithms/CompareTheTriplets.java
@@ -0,0 +1,66 @@
+package Algorithms;
+
+import java.io.*;
+import java.util.*;
+
+class Result {
+
+    /*
+     * Complete the 'compareTriplets' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY. The function accepts
+     * following parameters: 1. INTEGER_ARRAY a 2. INTEGER_ARRAY b
+     */
+
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        // Write your code here
+        List<Integer> result = Arrays.asList(0, 0);
+        for (int i = 0; i < 3; i++) {
+            if (a.get(i) > b.get(i))
+                result.set(0, result.get(0) + 1);
+            if (b.get(i) > a.get(i))
+                result.set(1, result.get(1) + 1);
+        }
+        return result;
+    }
+}
+
+public class CompareTheTriplets {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] aTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> a = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int aItem = Integer.parseInt(aTemp[i]);
+            a.add(aItem);
+        }
+
+        String[] bTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> b = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int bItem = Integer.parseInt(bTemp[i]);
+            b.add(bItem);
+        }
+
+        List<Integer> result = Result.compareTriplets(a, b);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
--- Algorithms/Runner.java
+++ Algorithms/Runner.java
@@ -1,11 +1,49 @@
 package Algorithms;
 
+import java.util.*;
+
 public class Runner {
+
     public static void main(String[] args) {
-        int[] i = { 3, 4, 7, 5, 6, 2, 1 };
-        InsertionSort_Part2.insertionSort(i);
-        for (int a : i) {
-            System.out.println(a);
+        Scanner input = new Scanner(System.in);
+        int[] arr = new int[input.nextInt()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = input.nextInt();
         }
+        input.nextLine();
+        int loop = input.nextInt();
+        input.nextLine();
+        for (int j = 0; j < loop; j++) {
+            String s = input.nextLine().trim();
+            int index = input.nextInt();
+            int[] arr2 = arr;
+            switch (s) {
+                case "Insert":
+                    arr2 = new int[arr.length + 1];
+                    for (int i = 0; i < index; i++) {
+                        arr2[i] = arr[i];
+                    }
+                    arr2[index] = input.nextInt();
+                    for (int k = index + 1; k < arr2.length; k++) {
+                        arr2[k] = arr[k - 1];
+                    }
+                    break;
+                case "Delete":
+                    arr2 = new int[arr.length - 1];
+                    for (int i = 0; i < index; i++) {
+                        arr2[i] = arr[i];
+                    }
+                    for (int k = index + 1; k < arr.length; k++) {
+                        arr2[k - 1] = arr[k];
+                    }
+                    break;
+            }
+            if (input.hasNext())
+                input.nextLine();
+            for (int t : arr2) {
+                System.out.print(t + " ");
+            }
+        }
+        input.close();
     }
 }
