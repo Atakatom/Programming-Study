7f76a724a6df00798212e52ebdef644e508a8bac
Atakan Yontar <atakanyontar@gmail.com>
2021-10-17 20:15:00 +0300

    solved with 100% speed

---
+++ LeetCode/MiddleOfLinkedList/solution.java
@@ -0,0 +1,36 @@
+package LeetCode.MiddleOfLinkedList;
+
+class Solution {
+
+    class ListNode {
+        int val;
+        ListNode
+
+        next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode n = head;
+        int size = 1;
+        while (n.next != null) {
+            n = n.next;
+            size++;
+        }
+        size /= 2;
+        while (size-- > 0)
+            head = head.next;
+        return head;
+    }
+}
