bff35a5a33445bbfb7d629f88e0f7796ff1c3a70
Atakan Yontar <atakanyontar@gmail.com>
2021-10-19 03:26:16 +0300

    update

--- Patika/Java102/main.java
+++ Patika/Java102/main.java
@@ -1,45 +1,45 @@
-package Patika.Java102;
+// package Patika.Java102;
 
-public class main {
-    public static void main(String[] args) {
-        MyList<Integer> liste = new MyList<>();
-        System.out.println("Liste Durumu : " + (liste.isEmpty() ? "Boş" : "Dolu"));
-        liste.add(10);
-        liste.add(20);
-        liste.add(30);
-        liste.add(40);
-        liste.add(20);
-        liste.add(50);
-        liste.add(60);
-        liste.add(70);
+// public class main {
+// public static void main(String[] args) {
+// MyList<Integer> liste = new MyList<>();
+// System.out.println("Liste Durumu : " + (liste.isEmpty() ? "Boş" : "Dolu"));
+// liste.add(10);
+// liste.add(20);
+// liste.add(30);
+// liste.add(40);
+// liste.add(20);
+// liste.add(50);
+// liste.add(60);
+// liste.add(70);
 
-        System.out.println("Liste Durumu : " + (liste.isEmpty() ? "Boş" : "Dolu"));
+// System.out.println("Liste Durumu : " + (liste.isEmpty() ? "Boş" : "Dolu"));
 
-        // Bulduğu ilk indeksi verir
-        System.out.println("Indeks : " + liste.indexOf(20));
+// // Bulduğu ilk indeksi verir
+// System.out.println("Indeks : " + liste.indexOf(20));
 
-        // Bulamazsa -1 döndürür
-        System.out.println("Indeks :" + liste.indexOf(100));
+// // Bulamazsa -1 döndürür
+// System.out.println("Indeks :" + liste.indexOf(100));
 
-        // Bulduğu son indeksi verir
-        System.out.println("Indeks : " + liste.lastIndexOf(20));
+// // Bulduğu son indeksi verir
+// System.out.println("Indeks : " + liste.lastIndexOf(20));
 
-        // Listeyi Object[] dizisi olarak geri verir.
-        Object[] dizi = liste.toArray();
-        System.out.println("Object dizisinin ilk elemanı :" + dizi[0]);
+// // Listeyi Object[] dizisi olarak geri verir.
+// Object[] dizi = liste.toArray();
+// System.out.println("Object dizisinin ilk elemanı :" + dizi[0]);
 
-        // Liste veri türünde alt bir liste oluşturdu
-        MyList<Integer> altListem = liste.subList(0, 3);
-        System.out.println(altListem.toString());
+// // Liste veri türünde alt bir liste oluşturdu
+// MyList<Integer> altListem = liste.subList(0, 3);
+// System.out.println(altListem.toString());
 
-        // Değerim listedeki olup olmadığını sorguladı
-        System.out.println("Listemde 20 değeri : " + liste.contains(20));
-        System.out.println("Listemde 120 değeri : " + liste.contains(120));
+// // Değerim listedeki olup olmadığını sorguladı
+// System.out.println("Listemde 20 değeri : " + liste.contains(20));
+// System.out.println("Listemde 120 değeri : " + liste.contains(120));
 
-        // Listeyi tamamen boşaltır ve varsayılan boyutuna çevirir
-        liste.clear();
-        System.out.println(liste.toString());
+// // Listeyi tamamen boşaltır ve varsayılan boyutuna çevirir
+// liste.clear();
+// System.out.println(liste.toString());
 
-    }
+// }
 
-}
+// }
