fdf24b12f590a0b37c161b06097150bc66a1d3ef
Atakan Yontar <atakanyontar@gmail.com>
2021-11-24 17:55:09 +0300

    studuying

---
+++ Labs/TicTacToe_Tree.java
@@ -0,0 +1,13 @@
+
+class TicTacToe_Tree {
+    static int board[] = new int[9];
+    static {
+        for (int i = 0; i < board.length; i++)
+            board[i] = -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
