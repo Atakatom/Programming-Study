8a6d98706dfcdc596047a6e04e58cf1e54f3af6b
atakan yontar <atakanyontar@gmail.com>
2022-02-09 16:21:48 +0300

    Calculatin avg grade | completed

---
+++ Patika/Java101/AverageGrade.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class AverageGrade{
+	public static void main(String[] args){
+		String[] courseNames = {"Mathematics","Physics","English","History","Chemistry","Music"};
+		int avgGrade = 0;
+
+		var scan = new Scanner(System.in);
+
+		for(int i = 0; i < courseNames.length; i++){
+			System.out.printf("%s grade: ",courseNames[i]);
+			avgGrade += scan.nextInt();
+		}
+
+		avgGrade = avgGrade / courseNames.length;
+
+		scan.close();
+		
+		System.out.println(avgGrade > 60 ? "Student has passed the class" : "Student has failed");
+	}
+}
