7e741c26c4911b6001748dbe290253466e0a66af
atakan yontar <atakanyontar@gmail.com>
2022-02-13 23:16:49 +0300

    Completed | Guessing numbers

---
+++ Patika/Java101/GuessingNumbers.java
@@ -0,0 +1,67 @@
+package Java101;
+
+//practise for https://app.patika.dev/moduller/java101/pratik-sayi-tahmin
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingNumbers {
+
+    public static void main(String[] args) {
+        var rand = new Random();
+        int number = rand.nextInt(100);
+
+        var inp = new Scanner(System.in);
+        int right = 0;
+        int selected;
+        int[] wrong = new int[5];
+        boolean isWin = false;
+        boolean isWrong = false;
+
+        System.out.println(number);
+        while (right < 5) {
+            System.out.print("Lütfen tahmininizi giriniz : ");
+            selected = inp.nextInt();
+
+            if (selected < 0 || selected > 99) {
+                System.out.println("Lütfen 0-100 arasında bir değer giriniz.");
+                if (isWrong) {
+                    right++;
+                    System.out.println("Çok fazla hatalı giriş yaptınız. Kalan hak : " + (5 - right));
+                } else {
+                    isWrong = true;
+                    System.out.println("Bir daha hatalı girişinizde hakkınızdan düşülecektir.");
+                }
+                continue;
+            }
+
+            if (selected == number) {
+                System.out.println("Tebrikler, doğru tahmin ! Tahmin ettiğini sayı : " + number);
+                isWin = true;
+                break;
+            } else {
+                System.out.println("Hatalı bir sayı girdiniz !");
+                if (selected > number) {
+                    System.out.println(selected + " sayısı, gizli sayıdan büyüktür.");
+                } else {
+                    System.out.println(selected + " sayısı, gizli sayıdan küçüktür.");
+                }
+
+                wrong[right++] = selected;
+                System.out.println("Kalan hakkı : " + (5 - right));
+            }
+        }
+
+        if (!isWin) {
+            System.out.println("Kaybettiniz ! ");
+            if (!isWrong) {
+                System.out.println("Tahminleriniz : " + Arrays.toString(wrong));
+            }
+        }
+
+        inp.close();
+
+    }
+}
+
