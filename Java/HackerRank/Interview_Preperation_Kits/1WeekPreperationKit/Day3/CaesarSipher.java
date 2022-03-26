05b2b3fbe9733e08aff83e0361fbd08df10c8285
Atakan Yontar <atakanyontar@gmail.com>
2022-03-26 18:18:06 +0300

    folder name change

---
+++ Java/HackerRank/Interview_Preperation_Kits/1WeekPreperationKit/Day3/CaesarSipher.java
@@ -0,0 +1,63 @@
+// link of the problem https://www.hackerrank.com/challenges/one-week-preparation-kit-caesar-cipher-1/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-three
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'caesarCipher' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. INTEGER k
+     */
+
+    public static String caesarCipher(String s, int k) {
+        k = k % 26;
+        char[] text = s.toCharArray();
+        for(int i = 0; i < text.length; i++){
+            if(Character.isLetter(text[i])){
+                int letter = text[i];
+                char controller = (Character.isUpperCase(text[i])) ? 'Z' : 'z';
+                letter+=k;
+                if(letter > (int)controller)
+                    letter = letter - 26;          
+                text[i] = (char)letter;
+            }
+        }
+        return String.valueOf(text);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String s = bufferedReader.readLine();
+
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = Result.caesarCipher(s, k);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
