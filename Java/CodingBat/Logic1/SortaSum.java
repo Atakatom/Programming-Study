c7df21a94df3be9c6bfed56a3290db4212f3a6ec
Atakan Yontar <atakanyontar@gmail.com>
2022-04-11 05:15:56 +0300

    upload : logic1 - sortaSum solution

---
+++ Java/CodingBat/Logic1/SortaSum.java
@@ -0,0 +1,12 @@
+// link of the problem https://codingbat.com/prob/p183071
+
+public class SortaSum {
+    
+    public int sortaSum(int a, int b) {
+        if(a+b<=20 && a+b>=10){
+          return 20;
+        }
+        return a+b;
+      }
+      
+}
