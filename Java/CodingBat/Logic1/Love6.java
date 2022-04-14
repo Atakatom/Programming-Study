76e5d5fc115813134963859e4db1b004fcd0e45d
Atakan Yontar <atakanyontar@gmail.com>
2022-04-14 15:32:11 +0300

    upload : logic1 - love6 solution

---
+++ Java/CodingBat/Logic1/Love6.java
@@ -0,0 +1,9 @@
+// link of the problem https://codingbat.com/prob/p137742
+
+public class Love6{
+
+    public boolean love6(int a, int b) {
+        return(a==6 || b==6 || Math.abs(a-b)==6 || a+b==6);
+    }
+
+}
