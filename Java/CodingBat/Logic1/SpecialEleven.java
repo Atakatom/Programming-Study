3653502267ca9cb14656cd5c310a0c533e8f8690
Atakan Yontar <atakanyontar@gmail.com>
2022-04-14 15:33:46 +0300

    upload : logic1 - specialEleven solution

---
+++ Java/CodingBat/Logic1/SpecialEleven.java
@@ -0,0 +1,9 @@
+// link of the problem https://codingbat.com/prob/p100962
+
+public class SpecialEleven {
+    
+    public boolean specialEleven(int n) {
+        return(n%11==0 || n%11==1);
+      }
+      
+}
