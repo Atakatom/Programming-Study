6d4058bad64201102372fe8dcfc2897600abd35a
Atakan Yontar <atakanyontar@gmail.com>
2022-04-15 18:48:50 +0300

    upload : logic1 - old35 solution

---
+++ Java/CodingBat/Logic1/Old35.java
@@ -0,0 +1,9 @@
+// link of the problem https://codingbat.com/prob/p159612
+
+public class Old35 {
+    
+    public boolean old35(int n) {
+        return((n%3==0 && n%5!=0) || (n%5==0 && n%3!=0));
+    }
+
+}
