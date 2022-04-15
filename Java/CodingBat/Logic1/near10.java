3ba8cc0871c670c2a23005333e34d9e17bdd0e51
Atakan Yontar <atakanyontar@gmail.com>
2022-04-15 18:52:03 +0300

    upload : logic1 - near10 solution

---
+++ Java/CodingBat/Logic1/near10.java
@@ -0,0 +1,8 @@
+// link of the problem 
+public class near10 {
+    
+    public boolean nearTen(int num) {
+        return(num%10==0 ||num%10==1 ||num%10==2 ||num%10==8 ||num%10==9);
+      }
+      
+}
