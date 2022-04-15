d9293e6e6a4aa9af9c296ef9d9640b73c2cb146f
Atakan Yontar <atakanyontar@gmail.com>
2022-04-15 18:49:37 +0300

    upload : logic1 - less20 solution

---
+++ Java/CodingBat/Logic1/Less20.java
@@ -0,0 +1,9 @@
+// link of the problem https://codingbat.com/prob/p133158
+
+public class Less20 {
+    
+    public boolean less20(int n) {
+        return(n%20==18 || n%20 ==19);
+      }
+      
+}
