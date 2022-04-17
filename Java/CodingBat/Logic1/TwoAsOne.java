9491f920179fd171ade51ecf77e3e8663c88ddc6
Atakan Yontar <atakanyontar@gmail.com>
2022-04-17 10:02:45 +0300

    upload : logic1 - twoAsOne solution

---
+++ Java/CodingBat/Logic1/TwoAsOne.java
@@ -0,0 +1,9 @@
+// link of the problem https://codingbat.com/prob/p113261
+
+public class TwoAsOne {
+    
+    public boolean twoAsOne(int a, int b, int c) {
+        return(a+b==c || b+c==a || a+c==b);
+    }
+      
+}
