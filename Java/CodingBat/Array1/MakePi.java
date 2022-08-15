f97e28b3d25cd9b433efa4709f9dc9baa1a63774
Atakan Yontar <atakanyontar@gmail.com>
2022-08-15 23:47:24 +0300

    upload : array1 - makePi solution

---
+++ Java/CodingBat/Array1/MakePi.java
@@ -0,0 +1,11 @@
+// link of the problem https://codingbat.com/prob/p167011
+
+public class MakePi {
+    public int[] makePi() {
+        int[] x = new int[3];
+        x[0]=3;
+        x[1]=1;
+        x[2]=4;
+        return x;
+    }  
+}
