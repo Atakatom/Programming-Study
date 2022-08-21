44093005ae14464a63bc7e87caa6a1bf4ea6d06c
Atakan Yontar <atakanyontar@gmail.com>
2022-08-21 23:57:58 +0300

    upload : array1 - sum2 solution

---
+++ Java/CodingBat/Array1/Sum2.java
@@ -0,0 +1,12 @@
+// link of the problem https://codingbat.com/prob/p190968
+public class Sum2 {
+    public int sum2(int[] nums) {
+        if(nums.length == 0){
+          return 0;
+        }
+        if(nums.length==1){
+          return nums[0];
+        }
+        return nums[0]+nums[1];
+      }
+}
