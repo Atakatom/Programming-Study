ceb264b9c79c68a7ebf166f44f0e2ff0ff1ce404
Atakan Yontar <atakanyontar@gmail.com>
2022-08-22 23:57:52 +0300

    upload : array1 - middleWay solution

---
+++ Java/CodingBat/Array1/MiddleWay.java
@@ -0,0 +1,9 @@
+// link of the problem https://codingbat.com/prob/p146449
+public class MiddleWay {
+    public int[] middleWay(int[] a, int[] b) {
+        int[] result = new int[2];
+        result[0] = a[1];
+        result[1] = b[1];
+        return result;
+    }
+}
