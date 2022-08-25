d8126290b3cd046afeb93d5dbf4afdfa8df65377
Atakan Yontar <atakanyontar@gmail.com>
2022-08-25 23:57:25 +0300

    upload : array1 - has23 solution

---
+++ Java/CodingBat/Array1/Has23.java
@@ -0,0 +1,12 @@
+// link of the problem https://codingbat.com/prob/p171022
+
+public class Has23 {
+    public boolean has23(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 2 || nums[i] == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
