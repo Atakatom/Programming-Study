48440c86781acedb802d98f2d7c53215c6f8ea9d
Atakan Yontar <atakanyontar@gmail.com>
2022-08-30 18:34:45 +0300

    upload : array1 - start1 solution

---
+++ Java/CodingBat/Array1/Start1.java
@@ -0,0 +1,10 @@
+// link of the problem https://codingbat.com/prob/p109660
+
+public class Start1 {
+    public int start1(int[] a, int[] b) {
+        int count=0;
+        if(a.length>0 && a[0]==1){count++;}
+        if(b.length>0 && b[0]==1){count++;}
+        return count;
+      }      
+}
